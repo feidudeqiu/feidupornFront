838ac8df8818db818341b021b3c4fd96fc2b70c3
wuyan <1021857669@qq.com>
2020-03-25 18:42:44 +0800

    将打包后的静态文件上传到github

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-              target: 'https://domain.api', //你要访问的服务器域名
+              target: 'http://127.0.0.1:8080/api', //你要访问的服务器域名
               changeOrigin: true, //允许跨域
               pathRewrite: {
                 '^/api': ''   //需要rewrite重写的,
